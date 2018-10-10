d3829a366420869150a217539254d319057e9ed3
RakAlex18 <novalex76@gmail.com>
2018-10-10 06:32:58 +0300

    first commit

---
+++ includes/header.php
@@ -0,0 +1,25 @@
+<header id='header' class='header'>
+    <div class="container-fluid">
+        <div class="row justify-content-sm-between justify-content-center align-items-center">
+            <div class="col-lg-3 col-sm-5 col-auto">
+                <img src="img/Logo_1.png" alt="logo" class="logo">
+            </div>
+            <div class="col-lg-4 col-sm-4 description text-center">
+                - всё про содержание, ремонт и строительство автомобильных дорог
+            </div>
+
+            <div class="col-lg-3 col-sm-3 offset-lg-2 col-auto">
+
+
+                <ul class="nav registr">
+                    <li class="nav-item registr__item">
+                        <a class="nav-link active" href="registration.php">Регистрация</a>
+                    </li>
+                    <li class="nav-item registr__item">
+                        <a class="nav-link" href="#">Войти</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
